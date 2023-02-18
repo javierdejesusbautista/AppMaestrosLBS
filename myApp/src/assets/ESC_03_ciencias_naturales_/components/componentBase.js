43d263dd805fa8b8cec174693e10e1e31cc5b6b7
Cesar Burgueño <nek.silverk54@gmail.com>
2023-02-18 12:13:46 -0700

    Libro actualizado
    fab button se movio

---
+++ myApp/src/assets/ESC_03_ciencias_naturales_/components/componentBase.js
@@ -0,0 +1,42 @@
+class componentBase extends HTMLElement { 
+
+	constructor(){
+		super();
+		this._itemLbs;
+		this._id = 0;
+        this._value = "";
+        this._pagina = 0;
+        this._unsubscribe = undefined;
+        this.attachShadow({ mode: 'open' });
+        //const url = (window.location != window.parent.location) ? document.referrer : document.location.href;
+        //console.log("url:",url);
+        this._isAndroid = window.location.protocol == "file:" ? true : false;
+        this.Visor = this._isAndroid ? parent.Visor :  Visor;
+	}
+
+
+	_saveData() {
+		console.log("_saveData");
+        const { usuario } = this.Visor.tokenUser;
+        const idLibro     = this.Visor.idLibro;
+        const claveLibro  = this._isAndroid ? parent.IDRViewer.config.fileName.replace(".pdf",'') : IDRViewer.config.fileName.replace(".pdf",'');
+
+        const widgetData = {
+            data: this._itemLbs.value, 
+            ejercicio : "0",
+            elemento: this._id,
+            estado : 2,
+            libroid : idLibro,
+            pagina : this._pagina
+        }
+
+        this.Visor.saveDataFireStore({...widgetData});
+        
+        /*this.Visor.dbFirestore.doc(`${usuario}/libros`)
+                         .collection(claveLibro)
+                         .doc(this._id)
+                         .set(widgetData)
+		.then(()=> console.log)
+        .catch(error => console.error("Error adding document: ", error));*/
+    }
+}
---
+++ myApp/src/assets/ESC_03_ciencias_naturales_/components/radioLbs/radioLbs.js
@@ -0,0 +1,113 @@
+//import htmlDOC from './player.html'
+class radioLbs extends componentBase {
+
+    constructor() {
+      super();
+    }
+
+    async getData() {
+        var txt = '';
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = () => {
+          if(xmlhttp.status == 200 && xmlhttp.readyState == 4){
+            txt = xmlhttp.responseText;
+            this.shadowRoot.innerHTML = txt;
+            this._itemLbs = this.shadowRoot.querySelector("div");
+
+            this.shadowRoot.querySelector('slot[name="option"]').ontouchstart = (event) => {
+                // open/close the menu
+                this._logicData(event.target.parentNode,true);
+            };
+
+            this.updateStyle(this);
+          }
+        };
+        xmlhttp.open("GET","components/radioLbs/radioLbs.html",true);
+        xmlhttp.send();
+    }
+
+    _logicData(element,saveData){
+      const nodos = element.children;
+      const radio = element;
+
+      const divseleccionado=element.parentNode.querySelector("[seleccionado='true']");
+
+      if(divseleccionado == radio) return;
+
+      //Hace que todos los nodos hijos herede los estilos
+      for(let i =0; i<nodos.length; i++){
+            nodos[i].style.textDecoration="inherit";
+            nodos[i].style.textDecorationColor="inherit";
+            nodos[i].style.color="inherit";
+      }
+
+      if(divseleccionado){
+           divseleccionado.style.color="black";
+           divseleccionado.style.textDecoration="none";
+           divseleccionado.setAttribute("seleccionado","false");
+
+           radio.style.color="red"; 
+           radio.style.textDecoration="underline";
+           radio.style.textDecorationColor="black";
+           radio.setAttribute("seleccionado","true");
+      }
+      else {
+          radio.style.color="red";
+          radio.setAttribute("seleccionado","true");
+          radio.style.textDecoration="underline";
+          radio.style.textDecorationColor="black";
+      }
+
+      this._itemLbs.value = element.getAttribute("option");
+
+      if(saveData)  
+        this._saveData();
+    }
+
+    updateStyle(elem) {
+        const shadow = elem.shadowRoot;
+
+        shadow.querySelector("style").textContent = ``;
+
+        this._id = elem.getAttribute("id");
+        this._pagina = elem.getAttribute("pagina");
+        
+        this.observers();
+        this.loadData();
+    }
+
+    loadData(){
+        const search = this.Visor.store.getState().bookReducer.filter(a => a.elemento == this._id);
+
+        if(search != 0) {
+            this._itemLbs.value = search[0].data;
+            this._logicData(this.shadowRoot.querySelector('slot[name="option"]').assignedNodes()[this._itemLbs.value - 1],false);
+        } 
+    }
+
+    observers(){
+        //console.log("observers txtlbs");
+        this._unsubscribe = this.Visor.store.subscribe(()=>{
+            
+            const search = this.Visor.store.getState().bookReducer.filter(a => a.elemento == this._id);
+
+            if(search != 0) {
+                this._itemLbs.value = search[0].data;
+                this._logicData(this.shadowRoot.querySelector('slot[name="option"]').assignedNodes()[this._itemLbs.value - 1],false);
+            }
+
+        });
+    }
+
+    connectedCallback() {
+        this.getData();
+    }
+    disconnectedCallback() {
+        //this.shadowRoot.querySelector("button").removeEventListener('click', this._showModal);
+        //this.shadowRoot.querySelector("#btnFabButton").removeEventListener('click', this._showModal);
+        //this.shadowRoot.querySelector("input").removeEventListener('blur', this._saveData);
+        this._unsubscribe();
+    }
+}
+
+customElements.define('radio-lbs',radioLbs);
